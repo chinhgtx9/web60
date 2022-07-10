9a773ecc18a8a7b50733027e0ba1e3c75e10c84d
Chinhtm52 <54090319+chinhgtx9@users.noreply.github.com>
2022-07-11 01:32:07 +0700

    chap 3 -ud

--- chap3/btvn/bai2/http-server.js
+++ chap3/btvn/bai2/http-server.js
@@ -4,9 +4,9 @@ const server = http.createServer((req, res) => {
     if(req.url === '/') {
         res.end(`<h1>day la trang chu</h1>`);
     }else if(req.url === '/about'){
-        res.end('<h1>la thong tin ca nhan</h1>')
+        res.end(`<h1>la thong tin ca nhan</h1>`)
     }else{
-        res.end('<h1>duong dan nay khong ton tai</h1>')
+        res.end(`<h1>duong dan nay khong ton tai</h1>`)
     }
 })
 
